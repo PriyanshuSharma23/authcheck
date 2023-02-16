7599198643779044f8b1a2036630c77f10517ccd
Replit user <>
2023-02-16 10:48:58 +0000

    pre taquito

--- src/components/content/content.jsx
+++ src/components/content/content.jsx
@@ -3,59 +3,99 @@ import "./content.css";
 import { useState, useEffect } from "react";
 
 function Content() {
-  const [, setTagDetected] = useState(false);
+  // const [, setTagDetected] = useState(false);
   const [imageSrc, setImageSrc] = useState("./assets/3.png");
   const [status, setStatus] = useState("Tap to Verify");
-  const [verify, setVerify] = useState("Verify");
+  // const [verify, setVerify] = useState("Verify");
+  const [tokenID, setTokenID] = useState(null)
 
   useEffect(() => {
-    const handleNfcTagDetection = (event) => {
-      setTimeout(() => {
-        setTagDetected(true);
-        setImageSrc("./assets/gucci.png");
-        setStatus("Verified!!");
-        setVerify("Product detected and verified!");
-      }, 2000);
-    };
 
-    async function handleClick() {
-      if ("NDEFReader" in window) {
-        alert("NDEFReader is supported");
-        const ndef = new window.NDEFReader();
+    let NDEF;
+    if ("NDEFReader" in window) {
+      // eslint-disable-next-line no-undef
+      NDEF = new NDEFReader();
+      NDEF.scan().then(() => {
+        setStatus('Bring your phone close to the Tag!')
+      })
 
-        await ndef.scan();
+      NDEF.addEventListener("reading", (event) => {
+        // event.preventDefault();
+        // alert(event.preventDefault)
+        const message = event.message;
+        const records = message.records;
 
-        ndef.addEventListener("reading", ({ message, serialNumber }) => {
-          window.body = message;
-        });
-      }
+        const record = records[0];
+        if (record == null) {
+          setStatus('Argh! Cannot read data from the NFC tag. Try again?')
+          return;
+        }
+        const textDecoder = new TextDecoder(record.encoding);
+        const text = textDecoder.decode(record.data);
+
+        setStatus('Token Identified');
+        setTokenID(text);
+      });
+
+      NDEF.addEventListener("readingerror", () => {
+        setStatus('Argh! Cannot read data from the NFC tag. Try another one?')
+      });
     }
 
-    window.addEventListener("click", handleClick);
-
-    // Add event listener for NFC tag detection
-    // Replace 'nfc' with the appropriate event name for your use case
-    window.addEventListener("click", handleNfcTagDetection);
-
-    // Cleanup function to remove event listener
-    return () => {
-      window.removeEventListener("click", handleNfcTagDetection);
-      window.removeEventListener("click", handleClick);
-    };
-  }, []);
+  }, [])
 
   return (
     <div className="content">
-      <h1>{status}</h1>
+
       <div className="img">
         <img src={imageSrc} className="image" alt="" />
       </div>
-      <button>{verify}</button>
-      {/* {tagDetected && (
-              <p className='detected'>Product detected and verified!</p>
-            )} */}
+      <div>
+      <h1>{status}</h1>
+        {
+        tokenID && <p class="token-display">
+          {tokenID}
+        </p>
+        }
+      </div>
+    
     </div>
   );
 }
 
 export default Content;
+
+// <button
+//   onClick={async () => {
+//     if ("NDEFReader" in window) {
+//       // eslint-disable-next-line no-undef
+//       const NDEF = new NDEFReader();
+
+//       await NDEF.scan();
+//       setStatus('Bring your phone close to the Tag!')
+
+//       NDEF.addEventListener("reading", (event) => {
+//         // event.preventDefault();
+//         // alert(event.preventDefault)
+//         const message = event.message;
+//         const records = message.records;
+
+//         const record = records[0];
+//         if (record == null) {
+//           setStatus('Argh! Cannot read data from the NFC tag. Try again?')
+//           return;
+//         }
+//         const textDecoder = new TextDecoder(record.encoding);
+//         const text = textDecoder.decode(record.data);
+
+//         setStatus(text);
+//       });
+
+//       NDEF.addEventListener("readingerror", () => {
+//         setStatus('Argh! Cannot read data from the NFC tag. Try another one?')
+//       });
+//     }
+//   }}
+// >
+//   // {verify}
+// </button>
